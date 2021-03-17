807edc9e0eeb4dd640c64f9b5478f117279229c5
ibandura <ibandura@perpet.io>
2021-03-17 11:13:53 +0200

    Refactor router

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -5,37 +5,40 @@ import 'package:clubhouse/screens/sms/sms_screen.dart';
 import 'package:flutter/material.dart';
 
 class Routers {
-  static String home = '/home';
-  static String phone = '/phone';
-  static String sms = '/sms';
-  static String profile = '/profile';
+  static const String home = '/home';
+  static const String phone = '/phone';
+  static const String sms = '/sms';
+  static const String profile = '/profile';
 }
 
 // ignore: missing_return
 Route<dynamic> router(routeSetting) {
-  if (routeSetting.name == Routers.home)
-    return new MaterialPageRoute(
-      builder: (context) => HomeScreen(),
-      settings: routeSetting,
-    );
-
-  if (routeSetting.name == Routers.phone)
-    return new MaterialPageRoute(
-      builder: (context) => PhoneScreen(),
-      settings: routeSetting,
-    );
-
-  if (routeSetting.name == Routers.sms)
-    return new MaterialPageRoute(
-        builder: (context) => SmsScreen(
-              verificationId: routeSetting.arguments,
-            ),
-        settings: routeSetting);
-
-  if (routeSetting.name == Routers.profile)
-    return new MaterialPageRoute(
-        builder: (context) => ProfileScreen(
-              profile: routeSetting.arguments,
-            ),
-        settings: routeSetting);
+  switch (routeSetting.name) {
+    case Routers.home:
+      return new MaterialPageRoute(
+        builder: (context) => HomeScreen(),
+        settings: routeSetting,
+      );
+      break;
+    case Routers.phone:
+      return new MaterialPageRoute(
+        builder: (context) => PhoneScreen(),
+        settings: routeSetting,
+      );
+      break;
+    case Routers.sms:
+      return new MaterialPageRoute(
+          builder: (context) => SmsScreen(
+                verificationId: routeSetting.arguments,
+              ),
+          settings: routeSetting);
+      break;
+    case Routers.profile:
+      return new MaterialPageRoute(
+          builder: (context) => ProfileScreen(
+                profile: routeSetting.arguments,
+              ),
+          settings: routeSetting);
+      break;
+  }
 }

303daccc91d1b8d53c85e29703c3d0a8fc63428b
Cybinon <nikorunikk@gmail.com>
2023-03-27 12:28:08 +0800

    audio listening

--- lib/core/settings.dart
+++ lib/core/settings.dart
@@ -3,7 +3,7 @@ const APP_ID = '2a10ef884d5048f9a98d85dc0dec8937';
 
 /// Temporary token from the Agora console page
 const Token =
-    '007eJxTYAjkni4yJ+rJyW/L3t/6+vVl+OeDvxb8W7hHx3nCrurVN17WKTAYJRoapKZZWJikmBqYWKRZJlpapFiYpiQbpKQmW1gam2/3+J/cEMjIkL99ESsjAwSC+DwMJanFJbrJGYl5eak5DAwAUvMowQ==';
+    '007eJxTYJjhf123KnxD068Mk3UCAp5bwr3Vf6ueZlNfMXO5+46Vkc0KDEaJhgapaRYWJimmBiYWaZaJlhYpFqYpyQYpqckWlsbmZryKKQ2BjAxhOzsZGRkgEMTnYShJLS7RTc5IzMtLzWFgAADZQiCQ';
 
 /// Channel name inputed when generate temporary token
 const channelName = 'test-channel';
--- lib/screens/room/room_screen.dart
+++ lib/screens/room/room_screen.dart
@@ -1,13 +1,15 @@
 import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:clubhouse/models/models.dart';
 import 'package:clubhouse/screens/room/widgets/user_profile.dart';
-import 'package:clubhouse/core/data.dart';
+import 'package:clubhouse/services/authentication.dart';
 import 'package:clubhouse/utils/app_color.dart';
 import 'package:clubhouse/core/settings.dart';
 import 'package:clubhouse/widgets/rounded_button.dart';
-import 'package:clubhouse/widgets/rounded_image.dart';
-import 'package:flutter/material.dart' hide ModalBottomSheetRoute;
-import '../../core/settings.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import '../../core/data.dart';
+import '../../widgets/rounded_image.dart';
 
 /// Detail screen of selected room
 /// Initialize Agora SDK
@@ -32,16 +34,42 @@ class _RoomScreenState extends State<RoomScreen> {
   void initState() {
     super.initState();
     // Initialize Agora SDK
-    initialize();
+    setupVideoSDKEngine();
+  }
+
+  void join() async {
+    await agoraEngine.startPreview();
+    print("clicked");
+
+    // Set channel options including the client role and channel profile
+    ChannelMediaOptions options = ChannelMediaOptions(
+      // ignore: sdk_version_eq_eq_operator_in_const_context
+      clientRoleType: (AuthService().getUser().username == "guest")
+          ? ClientRoleType.clientRoleAudience
+          : ClientRoleType.clientRoleBroadcaster,
+      channelProfile: ChannelProfileType.channelProfileCommunication,
+    );
+
+    await agoraEngine.joinChannel(
+      token: token,
+      channelId: channelName,
+      options: options,
+      uid: uid,
+    );
+  }
+
+  void leave() {
+    setState(() {
+      _isJoined = false;
+      _remoteUid = null;
+    });
+    agoraEngine.leaveChannel();
   }
 
   @override
-  void dispose() {
-    // Clear users
-    _users.clear();
-    // Destroy sdk
-    _engine.leaveChannel();
-    // _engine.destroy();
+  void dispose() async {
+    await agoraEngine.leaveChannel();
+    agoraEngine.release();
     super.dispose();
   }
 
@@ -49,8 +77,12 @@ class _RoomScreenState extends State<RoomScreen> {
   Future<void> initialize() async {
     await _initAgoraRtcEngine();
     _addAgoraEventHandlers();
+    await [Permission.microphone, Permission.camera].request();
     await _engine.joinChannel(
-        token: Token, channelId: channelName, options: null, uid: 0);
+        token: Token,
+        channelId: channelName,
+        uid: 0,
+        options: ChannelMediaOptions(autoSubscribeAudio: true));
   }
 
   Future<void> _initAgoraRtcEngine() async {
@@ -59,6 +91,7 @@ class _RoomScreenState extends State<RoomScreen> {
       appId: APP_ID,
       channelProfile: ChannelProfileType.channelProfileLiveBroadcasting,
     ));
+    await _engine.enableVideo();
     await _engine.enableAudio();
     await _engine.setClientRole(role: widget.role);
   }
@@ -85,6 +118,61 @@ class _RoomScreenState extends State<RoomScreen> {
         });
       },
     ));
+  }
+
+  String token = Token;
+
+  int uid = 0; // uid of the local user
+
+  int _remoteUid; // uid of the remote user
+  bool _isJoined = false; // Indicates if the local user has joined the channel
+  RtcEngine agoraEngine; // Agora engine instance
+
+  final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
+      GlobalKey<ScaffoldMessengerState>(); // Global key to access the scaffold
+
+  showMessage(String message) {
+    scaffoldMessengerKey.currentState?.showSnackBar(SnackBar(
+      content: Text(message),
+    ));
+  }
+
+  Future<void> setupVideoSDKEngine() async {
+    // retrieve or request camera and microphone permissions
+    await [Permission.microphone, Permission.camera].request();
+
+    //create an instance of the Agora engine
+    agoraEngine = createAgoraRtcEngine();
+    await agoraEngine.initialize(const RtcEngineContext(appId: APP_ID));
+
+    await agoraEngine.enableVideo();
+
+    // Register the event handler
+    agoraEngine.registerEventHandler(
+      RtcEngineEventHandler(
+        onJoinChannelSuccess: (RtcConnection connection, int elapsed) {
+          showMessage(
+              "Local user uid:${connection.localUid} joined the channel");
+          setState(() {
+            _isJoined = true;
+          });
+        },
+        onUserJoined: (RtcConnection connection, int remoteUid, int elapsed) {
+          showMessage("Remote user uid:$remoteUid joined the channel");
+          setState(() {
+            _remoteUid = remoteUid;
+          });
+        },
+        onUserOffline: (RtcConnection connection, int remoteUid,
+            UserOfflineReasonType reason) {
+          showMessage("Remote user uid:$remoteUid left the channel");
+          setState(() {
+            _remoteUid = null;
+          });
+        },
+      ),
+    );
+    join();
   }
 
   @override
@@ -121,6 +209,43 @@ class _RoomScreenState extends State<RoomScreen> {
     );
   }
 
+// Display local video preview
+  // Widget _localPreview() {
+  //   if (_isJoined) {
+  //     return AgoraVideoView(
+  //       controller: VideoViewController(
+  //         rtcEngine: agoraEngine,
+  //         canvas: VideoCanvas(uid: 0),
+  //       ),
+  //     );
+  //   } else {
+  //     return const Text(
+  //       'Join a channel',
+  //       textAlign: TextAlign.center,
+  //     );
+  //   }
+  // }
+
+// Display remote user's video
+  Widget _remoteVideo() {
+    if (_remoteUid != null) {
+      return AgoraVideoView(
+        controller: VideoViewController.remote(
+          rtcEngine: agoraEngine,
+          canvas: VideoCanvas(uid: _remoteUid),
+          connection: RtcConnection(channelId: channelName),
+        ),
+      );
+    } else {
+      String msg = '';
+      if (_isJoined) msg = 'Waiting for a remote user to join';
+      return Text(
+        msg,
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+
   Widget body() {
     return Container(
       padding: const EdgeInsets.only(
@@ -149,6 +274,8 @@ class _RoomScreenState extends State<RoomScreen> {
                 others(
                   widget.room.users.sublist(widget.room.speakerCount),
                 ),
+                // _localPreview(),
+                _remoteVideo()
               ],
             ),
           ),
@@ -242,6 +369,7 @@ class _RoomScreenState extends State<RoomScreen> {
         children: [
           RoundedButton(
             onPressed: () {
+              leave();
               Navigator.pop(context);
             },
             color: AppColor.LightGrey,
